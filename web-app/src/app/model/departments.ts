785d168aefd27a2d0ea5398c20121ed20c2e9403
Илья Чайковский < >
2017-04-10 17:34:43 +0300

    Show

--- web-app/src/app/model/departments.ts
+++
@@ -1,9 +0,0 @@
-import { Employees }       from './employees';
-
-export class Departments {
-  constructor(
-    public id: number,
-    public departmentName: string,
-    public employeesInThisDepartment: Employees[],
-    public averageSalary: string) { }
-}
--- web-app/src/app/model/employees.ts
+++
@@ -1,9 +0,0 @@
-export class Employees {
-  constructor(
-    public id: number,
-    public departmentName: string,
-    public fullName: string,
-    public birthday: string,
-    public salary: string,
-    public department_id: string) { }
-}
